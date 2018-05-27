a611d6648c3f14e7d82eb74f5c7cecf5f7085b7e
matsushin11 <matsu1983@gmail.com>
2018-05-27 09:56:16 +0900

    投稿のコントラクトを追加

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Todos = artifacts.require("./Todos.sol");
+const PostToken = artifacts.require("./PostToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Todos);
+  deployer.deploy(PostToken);
 };
